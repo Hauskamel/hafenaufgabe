5e47fb3b9a9466d20929b22c5e7c1b7b6d08cabb
Hauskamel <lukas.krojer@gmx.de>
2024-02-19 18:42:38 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ship ship1 = new Ship("Ship1", 50, 100, 100);
+
+
+    }
+}
---
+++ src/Ship.java
@@ -0,0 +1,11 @@
+public class Ship {
+    public Ship (String name, int length, int v, int plastic) {
+        this.name = name;
+        this.length = length;
+        this.v = v;
+        this.plastic = plastic;
+    }
+
+
+
+}
